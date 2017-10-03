61f9c5c5bda70754c680b13b35274e978d56382a
omx1 <omx1@YESICA>
2017-10-02 20:55:09 -0500

    Add project files.

---
+++ App7/App7/Calculadora.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Calculadora : ContentPage
+    {
+        float uno;
+        float dos;
+        String op;
+        float rsl;
+
+        public Calculadora()
+        {
+
+            InitializeComponent();
+        }
+        private void Prin_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+        private void a_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "1";
+        }
+        private void b_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "2";
+        }
+        private void c_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "3";
+        }
+        private void d_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "4";
+        }
+        private void e_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "5";
+        }
+        private void f_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "6";
+        }
+        private void g_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "7";
+        }
+        private void h_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "8";
+        }
+        private void i_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "9";
+        }
+        private void j_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + "0";
+        }
+        private void punto_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = caja.Text + ".";
+        }
+        private void l_Clicked(object sender, EventArgs o)
+        {
+            double raiz = double.Parse(caja.Text);
+            double rl;
+            rl = Math.Sqrt(raiz);
+            caja.Text = "" + rl;
+        }
+        private void lim_Clicked(object sender, EventArgs o)
+        {
+            caja.Text = null;
+            uno = 0;
+            dos = 0;
+            op = null;
+        }
+        private void suma_Clicked(object sender, EventArgs o)
+        {
+            op = "+";
+            uno = float.Parse(caja.Text);
+            caja.Text = null;
+        }
+        private void r_Clicked(object sender, EventArgs o)
+        {
+            op = "-";
+            uno = float.Parse(caja.Text);
+            caja.Text = null;
+        }
+        private void o_Clicked(object sender, EventArgs o)
+        {
+            op = "*";
+            uno = float.Parse(caja.Text);
+            caja.Text = null;
+        }
+        private void k_Clicked(object sender, EventArgs o)
+        {
+            op = "/";
+            uno = float.Parse(caja.Text);
+            caja.Text = null;
+        }
+        private void q_Clicked(object sender, EventArgs e)
+        {
+
+            caja.Text = caja.Text + "%";
+            op = "%";
+            uno = float.Parse(caja.Text);
+            caja.Text = null;
+
+        }
+        private void p_Clicked(object sender, EventArgs o)
+        {
+            dos = float.Parse(caja.Text);
+            if (op.Equals("+"))
+            {
+                suma();
+            }
+            if (op.Equals("-"))
+            {
+                resta();
+            }
+            if (op.Equals("*"))
+            {
+                multi();
+            }
+            if (op.Equals("/"))
+            {
+                div();
+            }
+
+        }
+
+        private void div()
+        {
+            rsl = uno / dos;
+            caja.Text = "" + rsl;
+        }
+
+        private void multi()
+        {
+            rsl = uno * dos;
+            caja.Text = "" + rsl;
+        }
+
+        private void suma()
+        {
+            rsl = uno + dos;
+            caja.Text = "" + rsl;
+        }
+
+        private void resta()
+        {
+            rsl = uno - dos;
+            caja.Text = "" + rsl;
+        }
+
+    }
+}
---
+++ App7/App7/formulario.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class formulario : ContentPage
+    {
+        public formulario()
+        {
+            InitializeComponent();
+            
+        }
+        private void guardar_Clicked(object sender, EventArgs e)
+        {
+
+          DisplayAlert("Datos Ingresados", "Hola \n" + Nombre.Text + "\n" + Apellidos.Text + "" + Direccion.Text + "\n" + Telefono.Text + "\n" + Carrera.Text + "\n" + Semestre.Text + "\n" + Matricula.Text + "\n" + Correo.Text + "\n" + GitHub.Text, "OK");
+            Navigation.PushAsync(new Calculadora());
+        }
+    }
+}
